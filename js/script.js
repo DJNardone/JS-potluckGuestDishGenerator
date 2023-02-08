977220869ec6e03561cdbdb6210ad06dfb5cf5c6
dj_nardone <nardonedon@outlook.com>
2023-02-07 20:35:28 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const addGuestButton = document.querySelector(".invite");
+const guestInputLabel = document.querySelector(".add-guest label");
+const guestInput = document.querySelector(".add-guest input");
+const guestList = document.querySelector(".guest-list");
+const guestCount = document.querySelector(".attendance");
+const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  if (guest !== "") {
+    addToList(guest);
+    updateGuestCount();
+    clearInput();
+  }
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  let listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length >= 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  let potLuckItems = [
+    "soft drinks",
+    "spring rolls",
+    "lo mein",
+    "fried chicken",
+    "spaghetti",
+    "meatballs",
+    "mac & cheese",
+    "chips & dips",
+    "fruit & cheese",
+    "hummus & naan",
+    "cheesecake",
+    "flan"
+  ];
+
+  const allGuests = document.querySelectorAll(".guest-list li");
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potLuckItems.length);
+    let randomPotluckItem = potLuckItems[randomPotluckIndex];
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+    potLuckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
