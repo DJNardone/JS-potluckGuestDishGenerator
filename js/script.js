22496c132c1f088a6e5b1d1584fb375f7406112f
dj_nardone <nardonedon@outlook.com>
2023-02-07 20:50:37 -0500

    Added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+// invite button
 const addGuestButton = document.querySelector(".invite");
+// label for the invite button
 const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
 const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
 const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+//assign dishes button
 const assignButton = document.querySelector(".assign");
+//assigned potluck dishes list
 const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
